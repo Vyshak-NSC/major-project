9668bca1eb19517aa60bea723d2c1bad591aa2d7
Abhinav Dharmaraj <abhinavdharmaraj9@gmail.com>
2025-03-26 14:28:06 +0530

    corrected css.2nd presentation

--- app/blueprints/local_auth/routes.py
+++ app/blueprints/local_auth/routes.py
@@ -14,3 +14,8 @@ def index():
 def get_camp_details():
     data = CampManager.get_camp_data(current_user.associated_camp_id)
     return jsonify(data)
+
+@local_auth_bp.route('/get_camp_list')
+def list_all_camps():
+    data = CampManager.list_all_camps()
+    return jsonify(data)
